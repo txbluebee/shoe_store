03e0ec31d9616aae1ac32191963232a3690d65a7
Brian Fan <kuomu.fan@yahoo.com>
2017-05-12 11:59:42 -0700

    add pages to let user to add stores and view individual store

--- app.rb
+++ app.rb
@@ -5,12 +5,24 @@ require("pry")
 ENV['RACK_ENV'] = 'test'
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
-
+#Home page
 get('/') do
   @stores = Store.all()
   erb(:index)
 end
-
-get('/') do
-
+#add new store
+get('/stores/new') do
+  erb(:store_form)
+end
+#post new store
+post('/stores/new') do
+  name = params.fetch('name')
+  store = Store.create({:name => name})
+  redirect('/')
+end
+#view individual store
+get('/stores/view/:id')do
+  store_id = params.fetch('id').to_i()
+  @store = Store.find(store_id)
+  erb(:store)
 end
