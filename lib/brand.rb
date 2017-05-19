f400651366756d4c9af8501831a6922757362ce8
Brian Fan <kuomu.fan@yahoo.com>
2017-05-19 09:29:23 -0700

    validate the uniqueness of the name to be case insensitive

--- lib/brand.rb
+++ lib/brand.rb
@@ -2,7 +2,8 @@ class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
   validates :name, :presence => true,
                    :length => {:minimum => 3, :maximum => 100},
-                   :uniqueness => true
+                   :uniqueness => {:case_sensitive => false}
+
   validates_numericality_of :price, :presence => true
 
   before_save :capitalize_name
--- lib/store.rb
+++ lib/store.rb
@@ -2,7 +2,7 @@ class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
   validates :name, :presence => true,
                    :length => {:minimum => 3, :maximum => 100},
-                   :uniqueness => true
+                   :uniqueness => {:case_sensitive => false}
   before_save :capitalize_name
 
   private
